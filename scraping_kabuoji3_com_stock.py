b57d70e1f4de75c2d55b8785c83a442132e98dbe
tetsuji37 <tetsuji.matsuo@zaisan.net>
2019-05-22 17:46:54 +0900

    証券コード(1部のみ)・年毎の リンクの存在確認

--- scraping_kabuoji3_com_stock.py
+++ scraping_kabuoji3_com_stock.py
@@ -3,7 +3,9 @@ from bs4 import BeautifulSoup
 import certifi
 import csv
 import re
+import time
 
+# ①最初に取得できる証券コードを取得する（ページ数の取得）
 # アクセスするURL(初期URL)
 url = "https://kabuoji3.com/stock/"
 
@@ -36,16 +38,17 @@ with open("test.csv", "w", encoding='utf-8') as file:
             writer.writerow(csvRow)
             # print(cell.get_text())
 
-
 # page記述のあるa タグを抽出
 ul = soup.find_all("a", href=re.compile("\?page="))
 print(ul)
 page_list = []
 for a in ul:
     page_list.append(a.string)
-    print(a.string)
+    # print(a.string)
 
+params =[]
 
+# ②取得データページ数で、各ページで証券コードを取得する
 for page in page_list:
     url = "https://kabuoji3.com/stock/?page=" + page
     
@@ -59,32 +62,70 @@ for page in page_list:
     
     # htmlをBeautifulSoupで扱う
     soup = BeautifulSoup(r.data, "html.parser")
+    time.sleep(10)
     
     # タイトル要素を取得する
     title_tag = soup.title
     
-    print(title_tag)
+    # print(title_tag)
     
     table = soup.findAll("table", {"class":"stock_table"})[0]
     rows = table.findAll("tr")
+
+    if page == '2':
+        break
     
-    with open("test" + page + ".csv", "w", encoding='utf-8') as file:
-        writer = csv.writer(file)
+    with open("stock_page_" + page + ".csv", "w", encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for row in rows:
             csvRow = []
+            col = 0
+            market = ""
+            code = ""
+            name = ""
             for cell in row.findAll(['td', 'th']):
-                csvRow.append(cell.get_text())
-            if (len(csvRow) > 0):
-                writer.writerow(csvRow)
+                if col == 0:
+                    code = cell.get_text()[:4]
+                    name = cell.get_text()[5:]
+                elif col == 1:
+                    market = cell.get_text()
+                else:
+                    print('code: ' + code + ';' + 'name: ' + name + '; market: ' + market)
+                    params.append([code, name, market])
+                    # csvRow.append([code, name, market])
+                    writer.writerow([code, name, market])
+                    # csvRow.append(cell.get_text())
+                    # if len(csvRow) > 0:
+                    #     # writer.writerow(csvRow)
+                    #
+                    break
+                col = col + 1
 
-    
- 
+# 各取得できた内容から、各個別の証券コードのデータを取得する
+# https://kabuoji3.com/stock/1543/
 
+for param in params:
+    print(param)
+    if param[0].isnumeric() and len(param[0]) == 4:
+        print(param[0])
+        print(param[1])
+        print(param[2])
+        if param[2] == '東証1部':
+            url = 'https://kabuoji3.com/stock/' + param[0] + '/'
 
+            # httpsの証明書検証を実行している
+            http = urllib3.PoolManager(
+                cert_reqs='CERT_REQUIRED',
+                ca_certs=certifi.where())
 
-# class MyTestCase(unittest.TestCase):
-#   def test_something(self):
-#     self.assertEqual(True, False)
-
-# if __name__ == '__main__':
-  # unittest.main()
+            # URLにアクセスする htmlが返ってくる
+            r = http.request('GET', url)
+            # htmlをBeautifulSoupで扱う
+            soup = BeautifulSoup(r.data, "html.parser")
+            year_list = []
+            ul = soup.find_all("a", href=re.compile("https?://[\w/:%#\$&\?\(\)~\.=\+\-]+/stock/[0-9]{4}/[0-9]{4}/"))
+            print(ul)
+            for year in ul:
+                year_list.append(year.string)
+                
+            print(year_list)
